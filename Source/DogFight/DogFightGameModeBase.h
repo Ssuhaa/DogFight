d7b6d85f7cade966d369ff900d128b2924912d9a
Ssuhaa <rlgk924@naver.com>
2022-12-13 18:25:28 +0900

    Initial commit

---
+++ Source/DogFight/DogFightGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DogFightGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOGFIGHT_API ADogFightGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
